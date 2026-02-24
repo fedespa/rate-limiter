63f640a33c563b2be86e9bd52fdab810735136e7
calnanteam <spagnolofederico3@gmail.com>
2026-02-24 18:43:35 -0300

    feat: add AuditableEntity base class with createdAt and updatedAt

---
+++ src/main/java/com/app/rate_limiter/common/config/audit/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.app.rate_limiter.common.config.audit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
