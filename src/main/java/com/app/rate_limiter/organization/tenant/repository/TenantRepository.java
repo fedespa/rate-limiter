9b17e2edd0efb0b6ae0a32ac6d72e5cc83e05d28
calnanteam <spagnolofederico3@gmail.com>
2026-02-24 19:06:02 -0300

    feat: add tenant entity, tenant service, tenant repository and v1__create_tenant_schema.sql

---
+++ src/main/java/com/app/rate_limiter/organization/tenant/repository/TenantRepository.java
@@ -0,0 +1,12 @@
+package com.app.rate_limiter.organization.tenant.repository;
+
+import com.app.rate_limiter.organization.tenant.model.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TenantRepository extends JpaRepository<Tenant, Long> {
+
+    boolean existsByName(String name);
+
+}
