0fa181cea08e8d8721124c5b0a0b405d4f56edde
calnanteam <spagnolofederico3@gmail.com>
2026-02-24 19:12:05 -0300

    feat: work in progress on tenant entity

--- src/main/java/com/app/rate_limiter/organization/tenant/model/Tenant.java
+++ src/main/java/com/app/rate_limiter/organization/tenant/model/Tenant.java
@@ -1,6 +1,9 @@
 package com.app.rate_limiter.organization.tenant.model;
 
+import com.app.rate_limiter.common.model.AuditableEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -8,14 +11,17 @@ import lombok.*;
 @Builder
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
-public class Tenant A{
+public class Tenant extends AuditableEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
 
+    @Enumerated(EnumType.STRING)
+    @NotNull
     private TenantStatus status;
 
 }
